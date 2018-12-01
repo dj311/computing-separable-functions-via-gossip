42d914617abb1112f8a04dc6f81509d8f5b59622
djwj <daniel@wilshirejones.com>
2018-12-01 23:00:10 +0000

    initial attempt at animation with plotly

--- computing-separable-functions-with-gossip.py
+++ computing-separable-functions-with-gossip.py
@@ -1,4 +1,6 @@
+import random
 import itertools
+import matplotlib
 import networkx
 import numpy
 import scipy
@@ -6,104 +8,150 @@ import scipy
 from plotly.offline import plot
 
 
-# Setup the graph ----------------------------------------------------------------
+def plot_figure(graph):
+    nodes = {
+        'type': 'scatter',
+        'x': [x for x, y in graph.nodes],
+        'y': [y for x, y in graph.nodes],
+        'mode': 'markers',
+        'marker': {
+            'size': 10,
+            'color': [node_attributes['color'] for node_attributes in graph.nodes.values()],
+        },
+        'hoverinfo': 'none',
+    }
 
-# Use NetworkX for graphs (https://networkx.github.io/documentation/stable/tutorial.html)
-graph = networkx.generators.grid_2d_graph(20, 20)
+    edge_xs = sum(
+        [[start[0], end[0], None] for start, end in graph.edges],
+        [],
+    )
+    edge_ys = sum(
+        [[start[1], end[1], None] for start, end in graph.edges],
+        [],
+    )
 
-# Give nodes starting colors:
-b = '#1b3fae'
-g = '#3b9e1c'
-y = '#d0c100'
-r = '#ab1717'
+    edges = {
+        'type': 'scatter',
+        'mode': 'lines',
+        'x': edge_xs,
+        'y': edge_ys,
+        'line': {
+            'width': 2,
+            'color': '#eee',
+        },
+        'hoverinfo': 'none',
+    }
 
-initial_colors = [
-    b, b, b, b, y, y, y, y, b, b, b, r, b, r, b, b, b, y, y, y,
-    b, b, b, b, y, y, y, y, b, b, b, b, b, b, b, b, b, y, y, y,
-    b, b, b, b, y, y, y, r, b, b, b, b, b, r, b, b, b, b, b, b,
-    b, b, b, b, y, y, y, y, y, y, b, b, b, b, b, b, b, b, b, b,
-    b, b, b, b, y, y, y, y, y, y, b, b, b, b, b, b, b, b, b, b,
-    b, b, b, b, y, y, y, y, y, y, y, y, y, y, b, b, b, b, g, g,
-    b, b, b, b, y, y, y, y, y, y, y, y, y, y, y, b, b, g, g, g,
-    b, b, b, b, b, b, y, y, y, y, y, y, y, y, y, b, b, g, g, g,
-    b, b, b, b, b, b, y, y, y, y, y, y, y, b, b, b, b, b, g, g,
-    b, b, b, b, b, b, y, r, y, y, y, b, b, b, b, b, b, b, b, b,
-    b, b, b, b, b, b, y, y, y, y, y, b, b, b, b, b, b, b, b, b,
-    b, b, b, b, y, y, y, y, y, y, b, b, b, r, b, b, b, r, b, b,
-    b, b, b, b, y, y, y, r, y, y, b, b, b, b, b, b, b, b, b, b,
-    b, b, b, b, y, y, y, y, y, y, b, b, b, b, b, b, b, b, y, b,
-    b, g, g, b, y, y, y, y, y, b, b, b, b, b, b, b, b, y, y, y,
-    b, g, g, b, y, y, y, y, b, b, b, b, b, b, b, b, y, y, y, y,
-    b, b, b, b, y, y, y, y, b, b, b, r, b, y, y, y, y, y, y, b,
-    b, b, b, b, y, y, y, y, b, b, b, y, y, y, y, y, y, b, b, b,
-    y, y, y, y, y, y, y, y, b, b, b, b, y, y, y, b, b, b, r, r,
-    y, y, y, y, y, y, y, y, b, b, b, b, b, b, b, b, b, b, r, r,
-]
+    figure = {'data': [edges, nodes]}
 
-for node_attributes, color in zip(graph.nodes.values(), colors):
-    node_attributes['color'] = color
-
-
-# Plot the nodes -----------------------------------------------------------------
-
-plot_nodes = {
-    'type': 'scatter',
-    'x': [x for x, y in graph.nodes],
-    'y': [y for x, y in graph.nodes],
-    'mode': 'markers',
-    'marker': {
-        'size': 10,
-        'color': [node_attributes['color'] for node_attributes in graph.nodes.values()],
-    },
-    'hoverinfo': 'none',
-}
-
-edge_xs = sum(
-    [[start[0], end[0], None] for start, end in graph.edges],
-    [],
-)
-edge_ys = sum(
-    [[start[1], end[1], None] for start, end in graph.edges],
-    [],
-)
-
-plot_edges = {
-    'type': 'scatter',
-    'mode': 'lines',
-    'x': edge_xs,
-    'y': edge_ys,
-    'line': {
-        'width': 2,
-        'color': '#eee',
-    },
-    'hoverinfo': 'none',
-}
-
-plot_axis = {
-    'showline': False, # hide axis line, grid, ticklabels and  title
-    'zeroline': False,
-    'showgrid': False,
-    'showticklabels': False,
-    'title': '',
-}
-
-plot_layout = {
-    'title':  'Averaging Colours Over a Network with Gossip',
-    'font':  {'family': 'Lora'},
-    'autosize': True,
-    'showlegend': False,
-    'xaxis': plot_axis,
-    'yaxis': plot_axis,
-}
-
-plot_figure = {
-    'data': [plot_edges, plot_nodes],
-    'layout': plot_layout,
-}
-
-plot(plot_figure)
+    return figure
 
 
+if __name__ == '__main__':
+    # Setup the graph ----------------------------------------------------------------
 
-# Exponential Variable:
-#   scipy.stats.expon (https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.expon.html)
+    # Use NetworkX for graphs (https://networkx.github.io/documentation/stable/tutorial.html)
+    graph = networkx.generators.grid_2d_graph(20, 20)
+
+    # Give nodes starting colors:
+    b = '#1b3fae'
+    g = '#3b9e1c'
+    y = '#ffff00'
+    r = '#ab1717'
+
+    node_colors = [
+        b, b, b, b, y, y, y, y, b, b, b, r, b, r, b, b, b, y, y, y,
+        b, b, b, b, y, y, y, y, b, b, b, b, b, b, b, b, b, y, y, y,
+        b, b, b, b, y, y, y, r, b, b, b, b, b, r, b, b, b, b, b, b,
+        b, b, b, b, y, y, y, y, y, y, b, b, b, b, b, b, b, b, b, b,
+        b, b, b, b, y, y, y, y, y, y, b, b, b, b, b, b, b, b, b, b,
+        b, b, b, b, y, y, y, y, y, y, y, y, y, y, b, b, b, b, g, g,
+        b, b, b, b, y, y, y, y, y, y, y, y, y, y, y, b, b, g, g, g,
+        b, b, b, b, b, b, y, y, y, y, y, y, y, y, y, b, b, g, g, g,
+        b, b, b, b, b, b, y, y, y, y, y, y, y, b, b, b, b, b, g, g,
+        b, b, b, b, b, b, y, r, y, y, y, b, b, b, b, b, b, b, b, b,
+        b, b, b, b, b, b, y, y, y, y, y, b, b, b, b, b, b, b, b, b,
+        b, b, b, b, y, y, y, y, y, y, b, b, b, r, b, b, b, r, b, b,
+        b, b, b, b, y, y, y, r, y, y, b, b, b, b, b, b, b, b, b, b,
+        b, b, b, b, y, y, y, y, y, y, b, b, b, b, b, b, b, b, y, b,
+        b, g, g, b, y, y, y, y, y, b, b, b, b, b, b, b, b, y, y, y,
+        b, g, g, b, y, y, y, y, b, b, b, b, b, b, b, b, y, y, y, y,
+        b, b, b, b, y, y, y, y, b, b, b, r, b, y, y, y, y, y, y, b,
+        b, b, b, b, y, y, y, y, b, b, b, y, y, y, y, y, y, b, b, b,
+        y, y, y, y, y, y, y, y, b, b, b, b, y, y, y, b, b, b, r, r,
+        y, y, y, y, y, y, y, y, b, b, b, b, b, b, b, b, b, b, r, r,
+    ]
+
+    for node_attributes, color in zip(graph.nodes.values(), node_colors):
+        node_attributes['color'] = color
+
+
+    axis = {
+        'showline': False, # hide axis line, grid, ticklabels and  title
+        'zeroline': False,
+        'showgrid': False,
+        'showticklabels': False,
+        'title': '',
+    }
+
+    layout = {
+        'title':  'Averaging Colours Over a Network with Gossip',
+        'font':  {'family': 'Lora'},
+        'autosize': True,
+        'showlegend': False,
+        'xaxis': axis,
+        'yaxis': axis,
+        'updatemenus': [
+            {
+                'type': 'buttons',
+                'buttons': [
+                    {
+                        'label': 'Play',
+                        'method': 'animate',
+                        'args': [
+                            None,
+                            {
+                                'frame': {'duration': 1000, 'redraw': False},
+                                'mode': 'immediate',
+                                'fromcurrent': True,
+                                'transition': {'duration': 0, 'easing': 'linear'}
+                            }
+                        ],
+                    },
+                    {
+                        'label': 'Pause',
+                        'method': 'animate',
+                        'args': [
+                            None,
+                            {
+                                'frame': {'duration': 0, 'redraw': False},
+                                'mode': 'immediate',
+                                'transition': {'duration': 0, 'easing': 'linear'},
+                            }
+                        ],
+                    }
+                ],
+                'pad': {'r': 10, 't': 87},
+                'showactive': False,
+            }
+        ],
+    }
+
+    frames = [plot_figure(graph)]
+    for _ in range(20):
+        random.shuffle(node_colors)
+        for node_attributes, color in zip(graph.nodes.values(), node_colors):
+            node_attributes['color'] = color
+        frames.append(plot_figure(graph))
+
+
+    figure = {
+        'data': frames[0]['data'],
+        'layout': layout,
+        'frames': frames,
+    }
+
+    plot(figure)
+
+    # Exponential Variable:
+    # scipy.stats.expon (https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.expon.html)
